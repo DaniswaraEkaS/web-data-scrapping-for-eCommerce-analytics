3190549030fc01374c6586b25c4c5fe6efb4524d
DaniswaraEkaS <vdanniz@gmail.com>
2025-02-12 22:18:06 +0700

    update

---
+++ coda_P0M1_Daniswara_Eka_S.sql
@@ -0,0 +1,29 @@
+'''
+=================================================
+Milestones 1
+
+Nama  : Daniswara Eka Saputra
+Batch : CODA-RMT-002
+
+Program ini dibuat untuk memasukan data Sneakers_Kick_Avenue.csv ke dalam postgreSQL 
+=================================================
+'''
+
+
+
+CREATE TABLE Sneakers(
+   id SERIAL PRIMARY KEY,
+   nama_produk text, 
+   harga int,
+   gambar text,
+   link_produk text
+);
+
+COPY Sneakers(nama_produk, harga, gambar, link_produk)
+FROM 'C:\tmp\Sneakers_Kick_Avenue.csv'
+DELIMITER ','
+CSV HEADER;
+
+SELECT * FROM Sneakers
+
+-- database ini menurut saya sudah benar jadi tidak perlu dilakukan normalisasi
